e3d4e296c4043beb2bc68aaad12a9388d1d3d096
🇨🇳钟智强 『江西青垣科技』 <johnmelodymel@qq.com>
2026-02-08 23:43:08 +0800

    feat(model): 添加交易所枚举类型定义
    添加 Exchange 类型及常用交易所常量，为后续交易数据模块提供类型支持

---
+++ internal/model/exchange.go
@@ -0,0 +1,11 @@
+package model
+
+type Exchange string
+
+var (
+	ExchangeHK Exchange = "hk" // 香港交易所
+	ExchangeSZ Exchange = "sz" // 深圳交易所
+	ExchangeMY Exchange = "my" // 马来西亚交易所
+	ExchangeUS Exchange = "us" // 美国交易所
+	ExchangeUK Exchange = "uk" // 英国交易所
+)
